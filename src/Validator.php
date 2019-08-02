f9a5fe22345cabf6c3e16e0b1a388296c8200fa2
Matt Saladna <msaladna@apisnetworks.com>
2019-08-02 16:50:42 -0400

    API endpoint validation queries statistics

--- src/Validator.php
+++ src/Validator.php
@@ -24,7 +24,7 @@ class Validator implements ServiceProvider {
     {
         try
         {
-            (new Api())->do('GET', 'servers');
+            (new Api())->do('GET', 'statistics');
         }
         catch (RequestException $e)
         {
